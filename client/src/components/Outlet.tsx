a815ba6f33c10b45083c3105eac24c30e6f607bf
Ahmed Gavaz <93177410+ahmedgavaz@users.noreply.github.com>
2023-04-12 14:42:44 +0300

    Add files via upload

---
+++ client/src/components/Outlet.tsx
@@ -0,0 +1,25 @@
+import { Navigate, Outlet, useLocation } from "react-router";
+import { User } from "../services/auth";
+
+interface OutletProps{
+    user:User|null;
+}
+
+function NavigateToLogin(){
+    const location=useLocation();
+    return <Navigate to ="/login" state={{locationFrom:location.pathname}}/>
+}
+
+export function PrivateOutlet({user}:OutletProps){
+    return user ? <Outlet/> : <NavigateToLogin/>;
+}
+
+
+function NavigateToHome(){
+    const location=useLocation();
+    return <Navigate to ="/" state={{locationFrom:location.pathname}}/>
+}
+
+export function PublicOutlet({user}:OutletProps){
+    return !user ? <Outlet/> : <NavigateToHome/>;
+}

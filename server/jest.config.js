7cf30a75576cfd70a5ddfb314da91276a7c0b626
Ahmed Gavaz <93177410+ahmedgavaz@users.noreply.github.com>
2023-04-20 22:34:56 +0300

    Delete server directory

--- server/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/test/support/setup.ts'],
-  maxConcurrency: 1,
-  maxWorkers: 1
-};

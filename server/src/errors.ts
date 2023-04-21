600fc85c0be94ca71ac24e7c428f03a81a0c9bd0
Ahmed Gavaz <93177410+ahmedgavaz@users.noreply.github.com>
2023-04-21 12:01:06 +0300

    Delete server directory

--- server/src/errors.ts
+++
@@ -1,9 +0,0 @@
-export class BadRequestError extends Error {};
-
-export class NotFoundError extends Error {};
-
-export class UserAlreadyExistError extends Error {};
-
-export class NotEnoughParametersError extends Error{};
-
-export class NotFoundMovieError extends Error{};
